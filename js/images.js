8c910cb34dbd5efaffcd481289a6d38803cd15c1
Korede-Feynman <142304418+Korede-Feynman@users.noreply.github.com>
2023-09-20 09:20:13 +0100

    Update images.js

--- js/images.js
+++ js/images.js
@@ -1,7 +1,7 @@
 // I made this file to easily import images
 
 export const images = {
-  logo: '../images/logo.jpg',
+  logo: '.../images/logo.jpg',
   orangeVestWorker: 'https://images.unsplash.com/photo-1589939705384-5185137a7f0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   workerOnWoodenRailings: 'https://images.unsplash.com/photo-1587582423116-ec07293f0395?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
   whiteMedicalEquipment: 'https://images.unsplash.com/photo-1516549655169-df83a0774514?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
